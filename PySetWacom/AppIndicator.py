c4154e0621b0fc20de3196304f05a6f989ed00aa
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-06-12 20:16:20 +0100

    Remove explicit gi requirement for Nautilus

--- PySetWacom/AppIndicator.py
+++ PySetWacom/AppIndicator.py
@@ -34,7 +34,6 @@ import gi
 gi.require_version('Gtk', '3.0')
 gi.require_version('AppIndicator3', '0.1')
 gi.require_version('Notify', '0.7')
-gi.require_version('Nautilus', '3.0')
 
 from gi.repository import Gtk
 from gi.repository import AppIndicator3
