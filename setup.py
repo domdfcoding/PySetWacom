e39cce83a33e24ae91b4aacc85b0957e15324b23
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2025-05-27 10:24:34 +0100

    Update build, test and linter config.

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@
 # This file is managed by 'repo_helper'. Don't edit it directly.
 
 # stdlib
+import pathlib
+import shutil
 import sys
 
 # 3rd party
@@ -10,13 +12,22 @@ from setuptools import setup
 sys.path.append('.')
 
 # this package
-from __pkginfo__ import *  # pylint: disable=wildcard-import
+from create_dotdesktop import create_dotdesktop
+
+extras_require = {}
+
+repo_root = pathlib.Path(__file__).parent
+install_requires = (repo_root / "requirements.txt").read_text(encoding="UTF-8").split('\n')
+
+create_dotdesktop("0.1.8")
 
 setup(
-		data_files=data_files,
+		data_files=[("share/applications", ["PySetWacom.desktop"])],
 		description="A GUI utility for configuring buttons on graphics tablets and styli",
 		extras_require=extras_require,
 		install_requires=install_requires,
+		name="pysetwacom",
 		py_modules=[],
-		version=__version__,
 		)
+
+shutil.rmtree("PySetWacom.egg-info", ignore_errors=True)
